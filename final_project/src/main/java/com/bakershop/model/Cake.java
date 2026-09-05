8c81f1ed418a349dbf22cec33dd227f6aa5f3219
Draclyst <115409932+Draclyst@users.noreply.github.com>
2026-09-05 11:46:48 +0700

    Cake.java getters

--- final_project/src/main/java/com/bakershop/model/Cake.java
+++ final_project/src/main/java/com/bakershop/model/Cake.java
@@ -1,5 +1,36 @@
 package com.bakershop.model;
 
 public class Cake {
-    
+    private int id;
+    private String name;
+    private Double price;
+    private String imageUrl;
+
+    public Cake(){
+
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public Double getPrice(){
+        return price;
+    }
+    public void setPrice(Double price){
+        this.price=price;
+    }
+    public String getImageUrl(){
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl){
+        this.imageUrl=imageUrl;
+    }
 }
