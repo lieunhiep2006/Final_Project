f6f2206cad4dd1ba726350e5e7cfdfc440a1c619
Draclyst <115409932+Draclyst@users.noreply.github.com>
2026-09-05 15:52:02 +0700

    product controller

--- final_project/src/main/java/com/bakershop/controller/ProductController.java
+++ final_project/src/main/java/com/bakershop/controller/ProductController.java
@@ -1,5 +1,22 @@
 package com.bakershop.controller;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.bakershop.dao.CakeDAO;
 
-public class ProductController {
-    
+@WebServlet("/products")
+public class ProductController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        CakeDAO dao = new CakeDAO();
+        request.setAttribute("cakeList", dao.getAllCakesWithThumbnail());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/shop/product-list.jsp");
+        dispatcher.forward(request,response);
+    }
 }
